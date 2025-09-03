5a07de4cd1aae0c140b3ae307f4309e1eb678039
Kayhan Behdin <kbehdin@linkedin.com>
2025-09-02 17:49:20 -0700

    fixing ci?

--- src/fmchisel/__init__.py
+++ src/fmchisel/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
-logger = logging.getLogger(__name__)
-try:
-    from llmcompressor.modifiers.factory import ModifierFactory
+from llmcompressor.modifiers.factory import ModifierFactory
 
-    ModifierFactory._EXPERIMENTAL_PACKAGE_PATH = "fmchisel"
-    ModifierFactory.refresh()
-    logger.info("Registered FMCHISEL modifiers successfully.")
-except ImportError:
-    logger.info("llmcompressor not detected. FMCHISEL Modifiers will not be registered.")
+logger = logging.getLogger(__name__)
+# try:
+
+
+ModifierFactory._EXPERIMENTAL_PACKAGE_PATH = "fmchisel"
+ModifierFactory.refresh()
+logger.info("Registered FMCHISEL modifiers successfully.")
+# except ImportError:
+#     logger.info("llmcompressor not detected. FMCHISEL Modifiers will not be registered.")
